dcff2101748e5e6d7e221e54a31695e0cfacb8fd
erminastrano <114101321+erminastrano@users.noreply.github.com>
2025-09-21 22:21:23 -0400

    Update App.jsx
    Signed-off-by: erminastrano <114101321+erminastrano@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
 import Clients from './sections/Clients.jsx';
-import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
 
 const App = () => {
